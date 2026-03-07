0f4ddda467494ac07cd2106d057e4de728d24f7e
kittithuch <kittistreet.th@gmail.com>
2026-03-07 14:41:45 +0700

    add templates

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require ('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/", (req,res) => {
+// app.get("/", (req,res) => {
 
-    res.send('Hello Ktiithuch.W');
-}) 
+//     res.send('Hello GYN.N');
+// }) 
 
 app.listen(port, ()=>{
-    debug("Listening on port" + chalk.green(" : "+port));
+    console.log("Listening on port" + chalk.green(" : "+port));
+    // debug("Listening on port" + chalk.green(" : "+port));
 })
 
